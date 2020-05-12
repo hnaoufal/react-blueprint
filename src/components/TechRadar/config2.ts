33982d565d453dba0c5295fbd7310dc6272537ed
hnaoufal <hicham.naoufal@bosch-si.com>
2020-05-12 17:51:39 +0200

    commit config2

---
+++ src/components/TechRadar/config2.ts
@@ -0,0 +1,492 @@
+export const entries2 = [
+  {
+    "Technology": "Java",
+    "Tags": "backend, language",
+    "Quadrant": "Language",
+    "Ring": "Adopt",
+    "Comment": ""
+  },
+  {
+    "Technology": "Spring",
+    "Tags": "backend, framework",
+    "Quadrant": "Framework",
+    "Ring": "Adopt",
+    "Comment": "(or Hold vs. Sprint Boot?) (question)"
+  },
+  {
+    "Technology": "Spring Boot",
+    "Tags": "backend, framework",
+    "Quadrant": "Framework",
+    "Ring": "Adopt",
+    "Comment": ""
+  },
+  {
+    "Technology": "inubit BPM",
+    "Tags": "backend, framework, infrastructure, language",
+    "Quadrant": "Framework",
+    "Ring": "Hold",
+    "Comment": ""
+  },
+  {
+    "Technology": "Linux",
+    "Tags": "infrastructure",
+    "Quadrant": "Infrastructure",
+    "Ring": "Adopt",
+    "Comment": ""
+  },
+  {
+    "Technology": "Ansible",
+    "Tags": "infrastructure, language, deployment",
+    "Quadrant": "Infrastructure",
+    "Ring": "Adopt",
+    "Comment": ""
+  },
+  {
+    "Technology": "Python",
+    "Tags": "language, backend",
+    "Quadrant": "Language",
+    "Ring": "Adopt",
+    "Comment": ""
+  },
+  {
+    "Technology": "Bash",
+    "Tags": "language, infrastructure",
+    "Quadrant": "Language",
+    "Ring": "Adopt",
+    "Comment": ""
+  },
+  {
+    "Technology": "Gradle",
+    "Tags": "build, deployment, backend, frontend",
+    "Quadrant": "Infrastructure",
+    "Ring": "Assess/Trial",
+    "Comment": ""
+  },
+  {
+    "Technology": "Maven",
+    "Tags": "build, deployment, backend, frontend",
+    "Quadrant": "Infrastructure",
+    "Ring": "Adopt",
+    "Comment": ""
+  },
+  {
+    "Technology": "Kafka",
+    "Tags": "data, infrastructure, backend",
+    "Quadrant": "Data Management",
+    "Ring": "Assess/Trial",
+    "Comment": ""
+  },
+  {
+    "Technology": "ZooKeeper",
+    "Tags": "infrastructure, data, backend",
+    "Quadrant": "Infrastructure",
+    "Ring": "Assess/Trial",
+    "Comment": ""
+  },
+  {
+    "Technology": "Solr",
+    "Tags": "data, backend",
+    "Quadrant": "Data Management",
+    "Ring": "Adopt",
+    "Comment": ""
+  },
+  {
+    "Technology": "BIRT (report engine)",
+    "Tags": "backend, framework",
+    "Quadrant": "Framework",
+    "Ring": "Assess/Trial",
+    "Comment": ""
+  },
+  {
+    "Technology": "AsciiDoc",
+    "Tags": "language",
+    "Quadrant": "Language",
+    "Ring": "Adopt",
+    "Comment": ""
+  },
+  {
+    "Technology": "Jenkins pipelines",
+    "Tags": "infrastructure, language, build",
+    "Quadrant": "Infrastructure",
+    "Ring": "Adopt",
+    "Comment": ""
+  },
+  {
+    "Technology": "SonarQube",
+    "Tags": "build, backend, frontend",
+    "Quadrant": "Infrastructure",
+    "Ring": "Adopt",
+    "Comment": ""
+  },
+  {
+    "Technology": "OpenAPI (Swagger)",
+    "Tags": "language",
+    "Quadrant": "Language",
+    "Ring": "Adopt",
+    "Comment": ""
+  },
+  {
+    "Technology": "JavaScript",
+    "Tags": "language, frontend",
+    "Quadrant": "Language",
+    "Ring": "Adopt",
+    "Comment": ""
+  },
+  {
+    "Technology": "AngularJS (1.x)",
+    "Tags": "framework, frontend",
+    "Quadrant": "Framework",
+    "Ring": "Hold",
+    "Comment": ""
+  },
+  {
+    "Technology": "Angular (2.x+)",
+    "Tags": "framework, frontend",
+    "Quadrant": "Framework",
+    "Ring": "Trial",
+    "Comment": " (or enough usage for \"Adopt\"?) (question)"
+  },
+  {
+    "Technology": "ExtJS",
+    "Tags": "framework, frontend",
+    "Quadrant": "Framework",
+    "Ring": "Hold",
+    "Comment": "(question)"
+  },
+  {
+    "Technology": "ReactJS",
+    "Tags": "framework, frontend",
+    "Quadrant": "Framework",
+    "Ring": "Assess/Trial",
+    "Comment": "(question)"
+  },
+  {
+    "Technology": "Selenium",
+    "Tags": "testing, framework, frontend",
+    "Quadrant": "Framework",
+    "Ring": "Assess/Trial",
+    "Comment": ""
+  },
+  {
+    "Technology": "PhantomJS",
+    "Tags": "testing, framework, frontend",
+    "Quadrant": "Framework",
+    "Ring": "Assess/Trial",
+    "Comment": ""
+  },
+  {
+    "Technology": "Robot",
+    "Tags": "testing, framework, backend",
+    "Quadrant": "Framework",
+    "Ring": "Assess/Trial",
+    "Comment": ""
+  },
+  {
+    "Technology": "JUnit 4",
+    "Tags": "testing, framework",
+    "Quadrant": "Framework",
+    "Ring": "Hold",
+    "Comment": ""
+  },
+  {
+    "Technology": "JUnit 5",
+    "Tags": "testing, framework",
+    "Quadrant": "Framework",
+    "Ring": "Adopt",
+    "Comment": ""
+  },
+  {
+    "Technology": "Elasticsearch",
+    "Tags": "data, infrastructure, backend",
+    "Quadrant": "Data Management",
+    "Ring": "Assess/Trial",
+    "Comment": ""
+  },
+  {
+    "Technology": "Logstash",
+    "Tags": "data, infrastructure, backend",
+    "Quadrant": "Data Management",
+    "Ring": "Assess/Trial",
+    "Comment": ""
+  },
+  {
+    "Technology": "Kibana",
+    "Tags": "data, frontend",
+    "Quadrant": "Infrastructure",
+    "Ring": "Hold",
+    "Comment": ""
+  },
+  {
+    "Technology": "Grafana",
+    "Tags": "data, frontend",
+    "Quadrant": "Infrastructure",
+    "Ring": "Adopt",
+    "Comment": ""
+  },
+  {
+    "Technology": "Prometheus",
+    "Tags": "data, backend",
+    "Quadrant": "Data Management",
+    "Ring": "Adopt",
+    "Comment": ""
+  },
+  {
+    "Technology": "Docker",
+    "Tags": "infrastructure, build, backend",
+    "Quadrant": "Infrastructure",
+    "Ring": "Adopt",
+    "Comment": ""
+  },
+  {
+    "Technology": "Docker Swarm",
+    "Tags": "infrastructure, build",
+    "Quadrant": "Infrastructure",
+    "Ring": "Hold",
+    "Comment": ""
+  },
+  {
+    "Technology": "Kubernetes",
+    "Tags": "infrastructure, backend",
+    "Quadrant": "Infrastructure",
+    "Ring": "Assess/Trial",
+    "Comment": ""
+  },
+  {
+    "Technology": "Git",
+    "Tags": "infrastructure",
+    "Quadrant": "Infrastructure",
+    "Ring": "Adopt",
+    "Comment": ""
+  },
+  {
+    "Technology": "SVN",
+    "Tags": "infrastructure",
+    "Quadrant": "Infrastructure",
+    "Ring": "Hold",
+    "Comment": ""
+  },
+  {
+    "Technology": "Cassandra",
+    "Tags": "data, infrastructure, backend",
+    "Quadrant": "Data Management",
+    "Ring": "Assess/Trial",
+    "Comment": ""
+  },
+  {
+    "Technology": "Tomcat",
+    "Tags": "deployment, backend, infrastructure",
+    "Quadrant": "Infrastructure",
+    "Ring": "Adopt",
+    "Comment": ""
+  },
+  {
+    "Technology": "Untertow",
+    "Tags": "deployment, backend, infrastructure",
+    "Quadrant": "Infrastructure",
+    "Ring": "Assess/Trial",
+    "Comment": " (question) (in MGM/MDH we tend more to Tomcat, so Hold for us, but in IOX-wide it's a candidate)"
+  },
+  {
+    "Technology": "Netty",
+    "Tags": "deployment, backend, infrastructure",
+    "Quadrant": "Infrastructure",
+    "Ring": "Assess/Trial",
+    "Comment": " (because: usecase of reactive apps not possible with servlet based containers like Tomcat/Undertow)"
+  },
+  {
+    "Technology": "Apache HTTPD",
+    "Tags": "infrastructure, backend",
+    "Quadrant": "Infrastructure",
+    "Ring": "Adopt",
+    "Comment": ""
+  },
+  {
+    "Technology": "Ningx",
+    "Tags": "infrastructure, backend",
+    "Quadrant": "Infrastructure",
+    "Ring": "Adopt",
+    "Comment": ""
+  },
+  {
+    "Technology": "Zuul 1.x",
+    "Tags": "infrastructure, backend",
+    "Quadrant": "Infrastructure",
+    "Ring": "Assess/Trial",
+    "Comment": " (not developed anymore?)"
+  },
+  {
+    "Technology": "Zuul 2.x",
+    "Tags": "infrastructure, backend",
+    "Quadrant": "Infrastructure",
+    "Ring": "Assess/Trial",
+    "Comment": ""
+  },
+  {
+    "Technology": "Log4j 1.x",
+    "Tags": "backend, framework",
+    "Quadrant": "Framework",
+    "Ring": "Hold",
+    "Comment": ""
+  },
+  {
+    "Technology": "Log4j 2.x",
+    "Tags": "backend, framework",
+    "Quadrant": "Framework",
+    "Ring": "Adopt",
+    "Comment": ""
+  },
+  {
+    "Technology": "Logback",
+    "Tags": "backend, framework",
+    "Quadrant": "Framework",
+    "Ring": "Hold",
+    "Comment": " (according to our research (10s Google) there seems to be no advantage of useing Logback vs Log4j 2.x now)"
+  },
+  {
+    "Technology": "Slf4j",
+    "Tags": "backend, framework",
+    "Quadrant": "Framework",
+    "Ring": "Adopt",
+    "Comment": ""
+  },
+  {
+    "Technology": "Typescript",
+    "Tags": "language, frontend",
+    "Quadrant": "Language",
+    "Ring": "Adopt",
+    "Comment": ""
+  },
+  {
+    "Technology": "Oracle DB",
+    "Tags": "data, backend, infrastructure",
+    "Quadrant": "Data Management",
+    "Ring": "Hold",
+    "Comment": " (question) (currently often in use because operations aspects from customers are strong and these are more valid than special Oracle features for us (which we should avoid to be more agnostic and rather use Ansi SQL))"
+  },
+  {
+    "Technology": "H2/HSQL",
+    "Tags": "data, backend",
+    "Quadrant": "Data Management",
+    "Ring": "Adopt",
+    "Comment": " (test cases?)"
+  },
+  {
+    "Technology": "MySQL",
+    "Tags": "data, backend, infrastructure",
+    "Quadrant": "Data Management",
+    "Ring": "Hold",
+    "Comment": " (use MariaDB)"
+  },
+  {
+    "Technology": "MariaDB",
+    "Tags": "data, backend, infrastructure",
+    "Quadrant": "Data Management",
+    "Ring": "Adopt",
+    "Comment": ""
+  },
+  {
+    "Technology": "DB2",
+    "Tags": "data, backend, infrastructure",
+    "Quadrant": "Data Management",
+    "Ring": "Assess/Trial",
+    "Comment": ""
+  },
+  {
+    "Technology": "PostgreSQL",
+    "Tags": "data, backend, infrastructure",
+    "Quadrant": "Data Management",
+    "Ring": "Assess/Trial",
+    "Comment": ""
+  },
+  {
+    "Technology": "Azure",
+    "Tags": "infrastructure, backend",
+    "Quadrant": "Infrastructure",
+    "Ring": "Assess/Trial",
+    "Comment": " (not enough infos to put one of them to Adopt or the other one to Hold)"
+  },
+  {
+    "Technology": "AWS",
+    "Tags": "infrastructure, backend",
+    "Quadrant": "Infrastructure",
+    "Ring": "Assess/Trial",
+    "Comment": " (not enough infos to put one of them to Adopt or the other one to Hold)"
+  },
+  {
+    "Technology": "Syslog",
+    "Tags": "backend, infrastructure",
+    "Quadrant": "Infrastructure",
+    "Ring": "Hold",
+    "Comment": " (question) (depends on operations use-cases and customer willingness)"
+  },
+  {
+    "Technology": "Puppet",
+    "Tags": "infrastructure, deployment",
+    "Quadrant": "Infrastructure",
+    "Ring": "Hold",
+    "Comment": ""
+  },
+  {
+    "Technology": "Salt",
+    "Tags": "infrastructure, deployment",
+    "Quadrant": "Infrastructure",
+    "Ring": "Hold",
+    "Comment": ""
+  },
+  {
+    "Technology": "Chef",
+    "Tags": "infrastructure, deployment",
+    "Quadrant": "Infrastructure",
+    "Ring": "Hold",
+    "Comment": ""
+  },
+  {
+    "Technology": "Terraform",
+    "Tags": "infrastructure, deployment",
+    "Quadrant": "Infrastructure",
+    "Ring": "Assess/Trial",
+    "Comment": ""
+  },
+  {
+    "Technology": "Nexus",
+    "Tags": "infrastructure, build, deployment",
+    "Quadrant": "Infrastructure",
+    "Ring": "Adopt",
+    "Comment": " (because it's there and many use it and have access it, which is a big advantage in itself)"
+  },
+  {
+    "Technology": "IM",
+    "Tags": "infrastructure",
+    "Quadrant": "Infrastructure",
+    "Ring": "Adopt",
+    "Comment": " (but: unknown future, no open protocol, overengineered or simple use-cases)"
+  },
+  {
+    "Technology": "TSM",
+    "Tags": "data",
+    "Quadrant": "Data Management",
+    "Ring": "Adopt",
+    "Comment": ""
+  },
+  {
+    "Technology": "MAM",
+    "Tags": "data",
+    "Quadrant": "Data Management",
+    "Ring": "Adopt",
+    "Comment": ""
+  },
+  {
+    "Technology": "UI-Integrator",
+    "Tags": "infrastructure",
+    "Quadrant": "Infrastructure",
+    "Ring": "Hold",
+    "Comment": " (question) (UI expertise needed, if the use-case is easy to be replaced by alternatives)"
+  },
+  {
+    "Technology": "Keycloack",
+    "Tags": "infrastructure",
+    "Quadrant": "Infrastructure",
+    "Ring": "Assess/Trial",
+    "Comment": ""
+  }
+]
--- src/pages/app/App.tsx
+++ src/pages/app/App.tsx
@@ -4,9 +4,28 @@ import { TechRadar } from "../../components/TechRadar/TechRadar";
 import { useSelector, useDispatch } from "react-redux";
 import { entriesList, getEntries } from "../../features/entries/entriesSlice";
 import { configObject, getConfig } from "../../features/config/configSlice";
+import { entries2 } from "../../components/TechRadar/config2";
 
+// quadrant: number,
+// ring: number,
+// label: string,
+// active: boolean,
 
+// const mapper = 
 
+const mapper = (config: any, entries: any) => {
+  const quadrantIndex = ;
+  const ringIndex = ;
+
+  return entries.map((entry: any) => ({
+    label: entry.Techonolgy,
+    quadrant: ,
+    ring: ,
+    active: false,
+    moved: 0,
+    link: ''
+  }));
+}
 
 function App() {
   const entries = useSelector(entriesList) as any;
@@ -20,7 +39,7 @@ function App() {
 
   return (
     <Container>
-      <TechRadar config={{ ...config, entries: entries.map((a: any) => ({ ...a })) }} />
+      <TechRadar config={{ ...config, entries: mapper(config, entries2) }} />
     </Container>
   );
 }
